6186b5f06a24840001ca63e40dedcf7eae949492
formiga82 <formiga18@yahoo.com.br>
2015-03-09 14:08:43 -0300

    Modificações
    Modificações que incluem um "enquirer" correto e otimizado para não
    repetir perguntas.

--- enquirer.js
+++ enquirer.js
@@ -18,10 +18,43 @@ Enquirer.provide('IEnquirer', function (done) {
 });
 
 Enquirer.install(function (done) {
-  this.discover = function () {
-    console.warn('unimplemented');
-    return false;
-  }.bind(this);
-
-  done();
+  var asked = [];
+    this.discover = function () {
+        asked = [];
+        var todos = this.knowledgeBase.list();
+        var count = todos.length;
+        var isTheAnimal = false;
+        var j;
+        for(var i=0;isTheAnimal==false && i<count;i++){
+            isTheAnimal = true;
+            var questions = this.knowledgeBase.retrieve(todos[i]);
+            var question;
+            j=i;
+            for (question in questions) {
+                var verR = this.verAnswear(question);
+                if (verR === -1){
+                    var respo = this.responder.ask(question);
+                    if (respo !== questions[question]) isTheAnimal = false;
+                    this.saveAnswear(question,respo);
+                }else{
+                    if (verR !== questions[question]) isTheAnimal = false;
+                }
+            }
+        }
+        if (isTheAnimal)
+            return todos[j];
+        else
+            return false;
+    }.bind(this);
+    this.saveAnswear = function (question,answear) {
+        asked.push([question,answear]);
+    }.bind(this);
+    this.verAnswear = function (question) {
+        var tam = asked.length;
+        for(var i=0;i<tam;i++){
+            if (asked[i][0] === question) return asked[i][1];
+        }
+        return -1;
+    }.bind(this);
+    done();
 });
